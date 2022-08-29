65ab30e80941852c56a183967a075c10b909b7fd
RussianHacker137 <110288455+RussianHacker137@users.noreply.github.com>
2022-08-29 16:38:37 +0300

    Update and rename pudge.py to main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
 
-print("СВЕЖЕЕ МЯСО!!!!")
+print("FRESH MEAT!")
 
 with open('spam.txt', 'r') as file:
-    text = file.read()
-token = '969df25fdd94c38a8bd95bf5d7dbf81c48110ac9c2a4a03ec4fcdf92a8ddd0cc956c355fb6c7b0e2d7f04'
-group_id = 213450595
+    text = file.read() # spam text. make yor own with record.py
+  
+token = '' # add your token here
+group_id = 0 # add ID of your group here
 
 if __name__ == '__main__':
     pudge = vk_api.VkApi(token=token)
@@ -19,7 +20,7 @@ for event in LongPoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
         peer_id = event.message.peer_id
         from_id = event.message.from_id
-        for i in range(1000):
+        for i in range(1000): # limit of messages
             try:
                 pudge.method("messages.send", {"peer_id": peer_id, "message": text[:4096],
                                                "random_id": random.randint(0, 100000)})
