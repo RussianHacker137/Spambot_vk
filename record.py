dcd71accb5e7274188da4cc0e0ad263260bbc8e4
RussianHacker137 <110288455+RussianHacker137@users.noreply.github.com>
2022-08-29 16:39:38 +0300

    Update record.py

--- record.py
+++ record.py
@@ -1,4 +1,4 @@
-text = input("Введите текст для спама: ")
+text = input("Enter the spam text: ")
 with open('spam.txt', 'w') as file:
-    for x in range(1000):
+    for x in range(4096):
         file.write(text + " ")
